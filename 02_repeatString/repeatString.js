70f8580e003056fcf7b650ee9429881928a9854f
SR <shunshynne@gmail.com>
2024-04-30 13:52:25 -0400

    repeat string solution with all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(repeatStr, repeatNum) {
+    let newString = repeatStr;
+    console.log(repeatNum);
+    if (repeatNum == 0) {
+        return "";
+        
+    } else if (repeatNum < 0) {
+        return "ERROR";
+        
+    } else {
 
-};
+        console.log("did we keep going?");
+        for (let i = 0; i < repeatNum-1; i++) {
+            console.log(newString);
+            newString = newString.concat(repeatStr);
+        }
+        return newString;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
