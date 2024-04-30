694a19b74b6af6968e42e9d4b2d236b043bfdb84
SR <shunshynne@gmail.com>
2024-04-30 15:17:37 -0400

    reverse string solution with all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(someString) {
+    let reverseString = [];
+
+    for (let i = 0; i < someString.length; i++) {
+        reverseString.push(someString.charAt(i));
+        console.log(someString.charAt(i));
+    }
+
+    reverseString.reverse();
+    let reversedString = reverseString.join('');
+
+    console.log(reversedString);
+
+    return reversedString;
 
 };
 
