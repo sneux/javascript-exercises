6572d2d2bc3b46aa8ec918557ec0554e611962c9
SR <shunshynne@gmail.com>
2024-05-01 11:53:33 -0400

    remove element from array solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(anArray, ...args) {
+    const newArray = [];
 
-};
+    anArray.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
+    
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
